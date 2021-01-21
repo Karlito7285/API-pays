1c6ee5275225f39661c3f3029e661cde1b49236d
Karlito7285 <73641416+Karlito7285@users.noreply.github.com>
2021-01-21 10:12:32 +0100

    Ajout projet API-pays

---
+++ script.js
@@ -0,0 +1,45 @@
+const searchInput = document.getElementById("search");
+const results = document.getElementById("results");
+
+let countries;
+let searchTerm = ``;
+
+//API REQUEST
+const fetchCountries = async() =>{
+    countries = await fetch("https://restcountries.eu/rest/v2/all?fields=name;population;flag").then(res => res.json());
+
+    console.log(countries);
+};
+
+const showCountries =async() =>{
+    await fetchCountries();
+
+    results.innerHTML =(
+        
+        countries
+        .filter(country=> country.name.toLowerCase().includes(searchTerm.toLowerCase()))
+        .map(country =>(
+
+        `<li class="country-item">
+            <img class="country-flag" src="${country.flag}"/>
+            <h3 class="country-name">${country.name}</h3>
+            <div class="country-info">
+                <h2 class="country-population">${numberWithSpace(country.population)}</h2>
+                <h5 class="country-population-text">Habitants</h5>
+            </div>
+        </li>` 
+        )).join(``)
+    );
+};
+
+showCountries();
+
+function numberWithSpace(x){
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g,` `); //SEPARATEUR DE NOMBRE (MILLIERS)
+}
+
+//INPUT SETUP
+searchInput.addEventListener("input", (e) => 
+{searchTerm = e.target.value;
+showCountries()
+});
